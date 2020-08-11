16b4fa5f8c2719e7007e5d8f87158b18bdf3da4d
Bruno Pereira <brunopereira.19@hotmail.com>
2020-08-11 11:32:13 -0300

    css updated

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,6 @@ function changeImageSize() {
     let imageItem = document.getElementById('contato-img')
     let width = window.screen.width
     if (width < 500) {
-        imageItem.style.width = "20%"
+        imageItem.style.width = "25%"
     }
 }
